4e517d43d85481bf59add0bc425f21dda27ee3f5
2082212005 <63175053+2082212005@users.noreply.github.com>
2020-10-11 09:55:50 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,128 +7,13 @@
 
 using namespace std;
 
-const int maxn=1e4+5;
-
-map<string,string> mp;
-
-void init()
-{
-    mp.clear();
-    mp["BEGIN"]="Begin";
-    mp["END"]="End";
-    mp["FOR"]="For";
-    mp["IF"]="If";
-    mp["THEN"]="Then";
-    mp["ELSE"]="Else";
-    mp["IDENT"]="Ident";
-    mp["INT"]="Int";
-    mp[":"]="Colon";
-    mp["+"]="Plus";
-    mp["*"]="Star";
-    mp[","]="Comma";
-    mp["("]="LParenthesis";
-    mp[")"]="RParenthesis";
-    mp[":="]="Assign";
-    mp["UNKNOWN"]="Unknown";
-}
-bool isLetter(char c)
-{
-	if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
-        return true;
-    else
-        return false;
-}
-bool isDigit(char c)
-{
-	if(c>='0'&&c<='9')
-        return true;
-    else
-        return false;
-}
-
 int main()
 {
-    init();
-	FILE *fp = fopen("out.txt", "w");
 	fstream fs("in.txt");
 	stringstream ss;
 	ss << fs.rdbuf();
 	string s;
 	s=ss.str();
-	string str="";
-	for(int i=0;i<s.length();)
-    {
-        while(s[i]==' '||s[i]=='\n'||s[i]=='\t'||s[i]=='\r')
-            i++;
-        if(isLetter(s[i]))
-        {
-            str+=s[i++];
-            while(isLetter(s[i])||isDigit(s[i]))
-                str+=s[i++];
-            if(mp.find(str)!=mp.end())
-                fprintf(fp,"%s\n",mp[str].c_str());
-            else
-                fprintf(fp,"%s(%s)\n",mp["IDENT"].c_str(),str.c_str());
-            str="";
-        }
-        else if(isDigit(s[i]))
-        {
-            str+=s[i++];
-            while(isDigit(s[i]))
-                str+=s[i++];
-            int j=0;
-            if(str!="0")
-            {
-                for(j=0;str[j]=='0';j++)
-                    ;
-                str=str.substr(j);
-            }
-            fprintf(fp,"%s(%s)\n",mp["INT"].c_str(),str.c_str());
-            str="";
-        }
-        else if(s[i]==':'&&s[i+1]!='=')
-        {
-            fprintf(fp,"%s\n",mp[":"].c_str());
-            i++;
-        }
-        else if(s[i]==':'&&s[i+1]=='=')
-        {
-            fprintf(fp,"%s\n",mp[":="].c_str());
-            i+=2;
-        }
-        else if(s[i]=='+')
-        {
-            fprintf(fp,"%s\n",mp["+"].c_str());
-            i++;
-        }
-        else if(s[i]=='*')
-        {
-            fprintf(fp,"%s\n",mp["*"].c_str());
-            i++;
-        }
-        else if(s[i]==',')
-        {
-            fprintf(fp,"%s\n",mp[","].c_str());
-            i++;
-        }
-        else if(s[i]=='(')
-        {
-            fprintf(fp,"%s\n",mp["("].c_str());
-            i++;
-        }
-        else if(s[i]==')')
-        {
-            fprintf(fp,"%s\n",mp[")"].c_str());
-            i++;
-        }
-        else
-        {
-            fprintf(fp,"%s\n",mp["UNKNOWN"].c_str());
-            break;
-        }
-    }
-    fclose(fp);
+	printf("%s",s.c_str());
     return 0;
 }
-
-
